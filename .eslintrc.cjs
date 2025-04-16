3efd6c027a7590ce0d0e6122cdd6a15805f56074
김동호 <donghok@innogrid.com>
2025-04-16 15:44:13 +0900

    init: project setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,26 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "unused-imports", "import"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'unused-imports', 'import'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
   ],
   rules: {
-    "unused-imports/no-unused-imports": "warn",
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "import/order": [
-      "warn",
+    'unused-imports/no-unused-imports': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'import/order': [
+      'warn',
       {
-        groups: ["builtin", "external", "internal"],
-        pathGroups: [{ pattern: "@shared/**", group: "internal" }],
-        pathGroupsExcludedImportTypes: ["builtin"],
-        "newlines-between": "always",
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [{ pattern: '@shared/**', group: 'internal' }],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        'newlines-between': 'always',
       },
     ],
   },
-  ignorePatterns: ["dist", ".next", "node_modules"],
+  ignorePatterns: ['dist', '.next', 'node_modules'],
 };
